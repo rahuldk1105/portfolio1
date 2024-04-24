aa614e06f590ea1a01b22b0d5721cddb1e60b067
itsrahul_8 <149181581+rahuldk1105@users.noreply.github.com>
2024-04-24 12:57:26 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,13 @@
-document.addEventListener("mousemove", function(event) {
-  var cursor = document.querySelector(".custom-cursor");
-  var x = event.clientX;
-  var y = event.clientY;
-  cursor.style.transform = "translate(" + (x - 10) + "px, " + (y - 10) + "px)"; // Adjust offset as needed
-});
+        const cursor = document.querySelector('.cursor');
+
+        document.addEventListener('mousemove', e => {
+            cursor.setAttribute("style", "top: "+(e.pageY - 10)+"px; left: "+(e.pageX - 10)+"px;")
+        })
+
+        document.addEventListener('click', () => {
+            cursor.classList.add("expand");
+
+            setTimeout(() => {
+                cursor.classList.remove("expand");
+            }, 500)
+        })
